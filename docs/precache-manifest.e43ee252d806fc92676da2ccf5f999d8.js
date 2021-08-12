615a0ddb265460ef1a791417c74e581c237ffb2b
Vicente Carmona <carmonavn@gmail.com>
2021-08-12 15:26:15 -0400

    docs agregado

---
+++ docs/precache-manifest.e43ee252d806fc92676da2ccf5f999d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117ab90cb94c8f45e46e53e76fea4f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9961d0363db80acbb22",
+    "url": "/static/css/main.658ec261.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9961d0363db80acbb22",
+    "url": "/static/js/main.dbbaebd7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
